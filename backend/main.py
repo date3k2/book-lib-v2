2dab850d5da591cfca8ebfd673b3bce8cb3b1ef8
date3k2 <nguyenduydat047@gmail.com>
2023-05-01 09:33:35 +0700

    learning fastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.post("/items/")
+async def create_item(name: str, description: str | None = None):
+    if description:
+        return {"name": name, "description": description}
+    return {"name": name}
