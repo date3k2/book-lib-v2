6de26bd2a8d41d0ae191c7cda9a3f70bdb99abe8
date3k2 <nguyenduydat047@gmail.com>
2023-05-10 04:33:56 +0700

    learning fastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Body
+from pydantic import BaseModel
+from typing import Annotated
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+    grade: int
+    description: str | None = None
+
+
+class Item(BaseModel):
+    id: int
+    tags: list[str]
+
 
 app = FastAPI()
 
@@ -14,7 +29,27 @@ async def read_item(item_id: int):
 
 
 @app.post("/items/")
-async def create_item(name: str, description: str | None = None):
+async def create_item(
+    name: str,
+    description: Annotated[
+        str, Query(max_length=5, title="Create item for project", alias="dsc")
+    ] = None,
+):
     if description:
         return {"name": name, "description": description}
     return {"name": name}
+
+
+@app.post("/students/")
+async def create_student(student: Student):
+    return student
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=False)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+# command for running the server
+# uvicorn main:app --reload
